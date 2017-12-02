3189f26b04b3ec2b8f632b3a0146ca7a64a9e0af
Szymon Ratajczak <daniel.kruk1@interia.pl>
2017-12-02 18:30:24 +0100

    Add files via upload

---
+++ Data filtering function.php
@@ -0,0 +1,55 @@
+<?php
+
+
+public static function($var,$type)
+{
+	
+	$flags=null;
+	switch($type)
+	{
+		case 'url':
+		{
+			$filter=FILTER_SANITIZE_URL;
+			break;
+		}
+		
+		case 'int':
+		{
+			$filter=FILTER_SANITIZE_NUMBER_INT;
+			break;
+		}
+		
+		case 'float':
+		{
+			$filter=FILTER_SANITIZE_NUMBER_FLOAT;
+			$flags=FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND
+			break;
+		}
+		case 'email':
+		{
+			$filter=FILTER_SANITIZE_EMAIL;
+			$var=substr($var,0,254);
+			break;
+			
+		}
+		
+		
+		case 'string':
+		default:
+			$filter=FILTER_SANITIZE_STRING;
+			$flags=FILTER_FLAG_NO_ENCODE_QUOTES;
+			break;
+			
+		
+		
+		
+		
+		
+		
+	}
+	
+	$output=filter_var($var,$filter,$flags);
+	return($output);
+}
+
+/>
